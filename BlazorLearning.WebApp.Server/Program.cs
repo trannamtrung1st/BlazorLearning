148e8effdd6bca470706cab1f0c97ce2d23f3507
trannamtrung1st <trannamtrung1st@gmail.com>
2023-07-16 17:10:20 +0700

    Finish web app example

---
+++ BlazorLearning.WebApp.Server/Program.cs
@@ -0,0 +1,26 @@
+using BlazorLearning.WebApp.Server;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.MapRazorComponents<App>()
+    .AddWebAssemblyRenderMode();
+
+app.Run();
