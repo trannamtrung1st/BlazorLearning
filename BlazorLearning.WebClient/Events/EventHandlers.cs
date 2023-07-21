9701bf418196df4d47109bd19e9f3bb6f236c8e6
trannamtrung1st <trannamtrung1st@gmail.com>
2023-07-21 22:40:08 +0700

    Finish event handling

---
+++ BlazorLearning.WebClient/Events/EventHandlers.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorLearning.WebClient.Events
+{
+
+    [EventHandler("oncustomevent", typeof(CustomEventArgs), enableStopPropagation: true, enablePreventDefault: true)]
+    [EventHandler("oncustomdblclick", typeof(CustomDblClickEventArgs), enableStopPropagation: true, enablePreventDefault: true)]
+    public static partial class EventHandlers
+    {
+    }
+
+    public class CustomEventArgs : EventArgs
+    {
+        public string ElementHtml { get; set; }
+        public string Author { get; set; }
+    }
+
+    public class CustomDblClickEventArgs : EventArgs
+    {
+        public string ElementHtml { get; set; }
+        public string Author { get; set; }
+    }
+}
--- BlazorLearning.WebClient/wwwroot/BlazorLearning.WebClient.lib.module.js
+++ BlazorLearning.WebClient/wwwroot/BlazorLearning.WebClient.lib.module.js
@@ -4,4 +4,24 @@
 
 export function afterStarted(blazor) {
     console.log('BlazorLearning.WebClient.lib.module.js afterStarted');
+    blazor.registerCustomEventType('customevent', {
+        createEventArgs: customEventArgsCreator
+    });
+    blazor.registerCustomEventType('customdblclick', {
+        browserEventName: 'dblclick',
+        createEventArgs: event => {
+            return {
+                author: 'TNT',
+                elementHtml: event.target.outerHTML
+            };
+        }
+    });
+}
+
+function customEventArgsCreator(event) {
+    const customEvent = {
+        ...event.detail,
+        author: 'TNT'
+    };
+    return customEvent;
 }
